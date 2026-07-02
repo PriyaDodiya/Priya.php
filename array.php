de1484608ee0633a65756382c271251dafc6eaff
Priya Dodiya <priyadodiya07@gmail.com>
2026-07-02 10:48:17 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,23 @@
+<?php 
+ /* array*/
+
+ $a = array(1,2,3);
+ echo $a [0];
+ echo $a [1];
+ echo $a [2];
+
+ $b = array(1.1,1.2,1.3);
+ echo $b [0];
+ echo $b [1];
+ echo $b [2];
+
+ $c = array("au","rk","mu");
+ echo $c [0];
+ echo $c [1];
+ echo $c [2];
+
+ $x=array(10,"au",20.5);
+ echo $x [0];
+ echo $x [1];
+ echo $x [2];
+?>
